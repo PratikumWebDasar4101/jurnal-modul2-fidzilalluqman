92767552fffbe401a9a9f7e8fad608eafb543eb6
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-09-10 10:00:29 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,27 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+error_reporting(0);
+$angka = $_GET ['angka'];
+function piramid ($x, $y){
+	if ($x > 0) {
+	if ($y > 0) {
+		echo "* ";
+		$y--;
+		piramid($x,$y);
+	}
+	else{
+		echo "<br>";
+		$x--;
+		piramid($x, $x);
+		}
+	}
+}
+piramid ($angka, $angka)
 
+for ($i=0; $i < $angka ; $i++) { 
+	if ($i % 2 == 0) {
+		echo " $i = bilangan genap";
+	}else {
+		echo " $i = bilangan ganjil";
+	}
+}
 ?>
